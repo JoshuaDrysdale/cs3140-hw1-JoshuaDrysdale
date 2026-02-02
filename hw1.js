72ac41f1a74e74e16d39028a206fd2510fcd9591
Joshua <joshua.j.drysdale@gmail.com>
2026-02-01 21:45:17 -0500

    part 1 complete, average, min and max functions added. Also changed the print layout

--- hw1.js
+++ hw1.js
@@ -8,6 +8,34 @@ function sum(arr){
     return total;
 }
 
-console.log(sum([1,2,3]));
+
+function average (arr){
+    return (sum(arr)/arr.length);
+}
 
 
+function min(arr){
+    let smallest = arr[0];
+    for (let i=1; i<arr.length; i++){
+        if (smallest>arr[i]){
+            smallest = arr[i];
+        }
+    }
+    return smallest;
+}
+
+function max(arr){
+    let biggest = arr[0];
+    for (let i=1; i<arr.length; i++){
+        if (biggest<arr[i]){
+            biggest = arr[i];
+        }
+    }
+
+    return biggest;
+}
+
+console.log("the sum of an array [1,2,3] = "+sum([1,2,3]));
+console.log("the average of an array [1,2,3] = "+average([1,2,3]));
+console.log("the min of an array [3,1,2] = "+min([3,1,2]));
+console.log("the max of an array [3,1,2] = "+max([3,1,2]));
